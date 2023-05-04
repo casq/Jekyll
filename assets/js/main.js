0909f44bd28e70f98353a7de30d3fb0771cb2ab6
casq <andy@northwalesit.co.uk>
2023-05-04 23:05:19 +0100

    chatgpt yer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("this worked");
+// Define a function that logs a message to the console
+function testFunction() {
+    console.log('Your JavaScript file is working!');
+  }
+  
+  // Call the function when the page is loaded
+  document.addEventListener('DOMContentLoaded', testFunction);
+  
